907d4a0013b0cb4802191bc2ebefbb8ece7235d5
pabvald <valduncielpablo@gmail.com>
2021-08-07 17:33:36 +0200

    bengali baseline s1

---
+++ modules/utils.py
@@ -0,0 +1,13 @@
+import numpy as np
+from typing import List
+
+def print_length_statistics(sentences: List[str]):
+    """ Compute some statistics of the length of the sentences
+    :param sentences: list of sentences
+    """
+    lengths = [len(s) for s in sentences]
+    print(f"Num. of sentences = {len(lengths)}")
+    print(f"Avg. length = {np.mean(lengths)}")
+    print(f"Std. dev. of the length = {np.std(lengths)}")
+    print(f"Maximum length = {max(lengths)}")
+    print(f"Minimum length = {min(lengths)}")
